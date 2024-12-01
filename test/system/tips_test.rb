14183a0c177caf38fee207b2be2c8f1827eaf2d2
MikeMatovu <matovumike44@gmail.com>
2024-12-01 22:49:41 +0300

    update: tests

--- test/system/tips_test.rb
+++ test/system/tips_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class TipsTest < ApplicationSystemTestCase
   setup do
-    @valid_bill = 100
+    @valid_bill = 10000
     @valid_tip_percentage = 10
     @valid_number_of_people = 2
   end
@@ -10,56 +10,63 @@ class TipsTest < ApplicationSystemTestCase
   test "submitting the form with valid inputs" do
     visit root_path
 
-    fill_in "Bill ($)", with: @valid_bill
-    fill_in "tip_tip_percentage", with: @valid_tip_percentage
-    fill_in "Number of People", with: @valid_number_of_people
+    fill_in "bill", with: @valid_bill
+    fill_in "custom-tip", with: @valid_tip_percentage
+    fill_in "number_of_people", with: @valid_number_of_people
 
     click_on "CALCULATE"
 
-    assert_text "Tip Amount"
-    assert_text "Total"
-    assert_text "Per Person"
+    sleep 10
+    puts "Waiting for the asynchronous request to complete..."
+
+    # Wait for the asynchronous request to complete
+    assert_selector "#tip-amount", text: "0.00", wait: 10
+    assert_selector "#total", text: "0.00", wait: 10
+
+    # Log message after waiting
+    puts "Asynchronous request completed."
+
   end
 
   test "submitting the form with empty fields" do
     visit root_path
 
-    fill_in "Bill ($)", with: ""
-    fill_in "tip_tip_percentage", with: ""
-    fill_in "Number of People", with: ""
+    fill_in "bill", with: ""
+    fill_in "custom-tip", with: ""
+    fill_in "number_of_people", with: ""
 
     click_on "CALCULATE"
 
-    assert_text "Bill can't be blank"
-    assert_text "Tip percentage can't be blank"
-    assert_text "Number of people can't be blank"
+    assert_text "! Can't be zero"
+    assert_text "! Can't be zero"
+    assert_text "! Can't be zero"
   end
 
   test "submitting the form with negative values" do
     visit root_path
 
-    fill_in "Bill ($)", with: -100
-    fill_in "tip_tip_percentage", with: -10
-    fill_in "Number of People", with: -2
+    fill_in "bill", with: -100
+    fill_in "custom-tip", with: -10
+    fill_in "number_of_people", with: -2
 
     click_on "CALCULATE"
 
-    assert_text "Bill must be greater than 0"
-    assert_text "Tip percentage must be greater than or equal to 0"
-    assert_text "Number of people must be greater than 0"
+    assert_text "! Can't be zero"
+    assert_text "! Can't be zero"
+    assert_text "! Can't be zero"
   end
 
   test "submitting the form with zero values" do
     visit root_path
 
-    fill_in "Bill ($)", with: 0
-    fill_in "tip_tip_percentage", with: 0
-    fill_in "Number of People", with: 0
+    fill_in "bill", with: 0
+    fill_in "custom-tip", with: 0
+    fill_in "number_of_people", with: 0
 
     click_on "CALCULATE"
 
-    assert_text "Bill must be greater than 0"
-    assert_text "Tip percentage must be greater than or equal to 0"
-    assert_text "Number of people must be greater than 0"
+    assert_text "! Can't be zero"
+    assert_text "! Can't be zero"
+    assert_text "! Can't be zero"
   end
 end
