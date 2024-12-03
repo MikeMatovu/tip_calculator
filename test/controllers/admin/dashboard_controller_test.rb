756587e2fd28da4bfc7da7daef69a6799d9aaee2
MikeMatovu <matovumike44@gmail.com>
2024-12-03 09:38:55 +0300

    update: admin dashboard auth

--- test/controllers/admin/dashboard_controller_test.rb
+++ test/controllers/admin/dashboard_controller_test.rb
@@ -1,8 +1,19 @@
 require "test_helper"
 
 class Admin::DashboardControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @username = ENV["ADMIN_USERNAME"] || "admin"
+    @password = ENV["ADMIN_PASSWORD"] || "admin123"
+  end
+
   test "should get index" do
-    get admin_dashboard_index_url
+    get admin_dashboard_index_url, headers: { "HTTP_AUTHORIZATION" => basic_auth(@username, @password) }
     assert_response :success
+  end
+
+  private
+
+  def basic_auth(username, password)
+    ActionController::HttpAuthentication::Basic.encode_credentials(username, password)
   end
 end
